3eb55a84ca3c491530fe8f2d7e936c2fd08b50c5
Kunal Kapur <kunalkapur888@gmail.com>
2022-07-06 19:03:56 -0700

    Created tree node and added way to add nodes on tree

--- src/App.js
+++ src/App.js
@@ -1,6 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+class treeNode {
+  constructor(left, right, val) {
+    this.left = left;
+    this.right = right;
+    this.val = val
+  }
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function insertNode(root, givenNode) {
+  if (root == null) {
+    return
+  }
+  let currNode = root;
+  while (true) {
+    if (givenNode.val <= currNode.val) {
+      if (currNode.left == null) {
+        currNode.left = givenNode;
+        break;
+      }
+      currNode = currNode.left;
+    }
+    else {
+      if (currNode.left == null) {
+        currNode.left = givenNode;
+        break;
+      }
+      currNode = currNode.left;
+
+    }
+  }
+
+}
+
+function buildOrderedTree(nums) {
+  nodeVals = [];
+  bottom = 0;
+  top = 3;
+
+  prevNode = null;
+
+
+  for (let i = 0; i < nums; i++) {
+    let random = getRandomArbitrary(bottom, top);
+
+    const currNode = new treeNode(null, null, random);
+    if (i == 0) {
+
+    }
+
+    bottom = random + 1
+    top = bottom + 3
+  }
+
+}
+
 function App() {
   return (
     <div className="App">
