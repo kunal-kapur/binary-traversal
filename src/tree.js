e4b606c9c3a21a3e53c6a7ff66c5b5cde42309ac
Kunal Kapur <kunalkapur888@gmail.com>
2022-07-07 21:30:08 -0700

    Switched css layout to grid

---
+++ src/tree.js
@@ -0,0 +1,27 @@
+
+
+vals = document.querySelectorAll(".node-area");
+
+
+for (let i = 0; i < vals.length; i++) {
+
+    node = vals[i].querySelector(":scope > .node");
+    console.log(node);
+    console.log(vals[i]);
+    node.style.backgroundColor = "purple";
+
+    const line = document.createElement("div");
+
+    line.classList.add("line");
+
+    if (vals[i].classList.contains("left")) {
+        line.classList.add("left");
+    }
+    else if (vals[i].classList.contains("right")) {
+        line.classList.add("right");
+    }
+    vals[i].insertBefore(line, vals[i].firstChild);
+
+
+
+}
